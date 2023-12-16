6ef8797b47e8b3ac818022f4287190b7233120b3
MohammadrezaAmani <More.Amani@yahoo.com>
2023-12-16 16:53:49 +0330

    import performance

--- get_data.py
+++ get_data.py
@@ -1,70 +1,78 @@
 import aiohttp
 import asyncio
 import json
+import os
 import re
 import requests
-import os
 
 IDS = [
-    'currency',
-    'gold-chart',
-    'coin',
-    'قیمت-سکه-پارسیان',
-    'نرخ-ارز-نیمایی',
-    'sana',
-    'bank',
-    'currency',
-    'currency-minor',
-    'global-stocks',
-    'world-market/currency',
-    'diff',
-    'commodities',
-    'energy',
-    'basemetal',
-    'gold-global',
-    'transfer',
+    "currency",
+    "gold-chart",
+    "coin",
+    "قیمت-سکه-پارسیان",
+    "نرخ-ارز-نیمایی",
+    "sana",
+    "bank",
+    "currency",
+    "currency-minor",
+    "global-stocks",
+    "world-market/currency",
+    "diff",
+    "commodities",
+    "energy",
+    "basemetal",
+    "gold-global",
+    "transfer",
 ]
 
 BASE_URL = "https://api.tgju.org/v1/market/indicator/summary-table-data/{}"
-data_text = []
-
 regex = re.compile(r'href="profile/(.*?)">')
-def get_id(id):
+
+
+async def get_id(id, data_text):
     data = []
     text = requests.get(f"https://www.tgju.org/{id}").text
     for i in regex.findall(text):
         i = i.replace(' target="_blank', "")
         i = i.replace('"', "", i.count('"'))
         if i.strip() not in data_text:
-            data_text.append(i.strip())
+            data_text.add(i.strip())
             data.append(i.strip())
     return data
 
 
-async def get_price(id: str, folder: str):
+async def get_price(session, id, folder):
     try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(BASE_URL.format(id)) as resp:
-                if resp.status == 200:
-                    data = await resp.json()
-                    with open(f"datasets/{folder.replace('/','-')}/{id}.json", "w", encoding="utf-8") as f:
-                        f.write(json.dumps(data, indent=4))
-                    print("done", id)
-                else:
-                    print("error", id)
+        async with session.get(BASE_URL.format(id)) as resp:
+            if resp.status == 200:
+                data = await resp.json()
+                async with aiofiles.open(
+                    f"datasets/{folder.replace('/', '-')}/{id}.json",
+                    "w",
+                    encoding="utf-8",
+                ) as f:
+                    await f.write(json.dumps(data, indent=4))
+                print("done", id)
+            else:
+                print("error", id)
     except Exception as e:
         print(e, id)
 
 
 async def main():
-    tasks = []
-    for i in IDS:
-        list_dir = os.listdir('datasets')
-        if i.replace('/','-') not in list_dir:
-            os.mkdir('datasets/'+i.replace('/','-'))
-        for j in get_id(i):
-            tasks.append(asyncio.create_task(get_price(j, i)))
-    await asyncio.gather(*tasks)
+    data_text = set()
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for i in IDS:
+            list_dir = os.listdir("datasets")
+            if i.replace("/", "-") not in list_dir:
+                os.mkdir("datasets/" + i.replace("/", "-"))
+            for j in await get_id(i, data_text):
+                tasks.append(get_price(session, j, i))
+        await asyncio.gather(*tasks)
 
 
-asyncio.run(main())
+if __name__ == "__main__":
+    import aiofiles
+
+    asyncio.run(main())
